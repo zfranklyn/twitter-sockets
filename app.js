4177ae0af40968dac4d88c43d4aba72cf47a3964
Sia <schen22@gmail.com>
2016-09-20 10:17:33 -0400

    recent changes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var Express = require('Express');
 var app = new Express();
 var morgan = require('morgan');
-
+var nunjucks = require('nunjucks');
+var tweetBank = require('./tweetBank.js');
+var routes = require('./routes/');
+app.use('/', routes);
 
 // app.use(function(req, res, next){
 //   console.log(req['method'] + " " + req['url']);
@@ -15,11 +18,36 @@ var morgan = require('morgan');
 
 app.use(morgan(':method :url :status'));
 
-
-app.get("/", function(req, res, next) {
-  console.log("server listening");
-  res.sendStatus(200);
+nunjucks.configure('views', {
+  autoescape: true,
+  noCache: true,
+  express: app
 })
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+
+var localsVar = {
+  title: "The Bleeding Operating Room",
+  people: [
+    {"name" : "Scalpel"},
+    {"name" : "Oxygen Deprivations"}
+    ]
+}
+
+// app.get('/', function(req, res) {
+//   res.render('index.html', localsVar);
+// })
+
+// app.get("/", function(req, res, next) {
+  // console.log("server listening");
+  // res.sendStatus(200);
+// })
+
+tweetBank.add("Sophia", "Hello Franklyn");
+console.log(tweetBank.find({name: "Sophia"})[0]["content"]);
+
 
 
 app.listen(3000);
